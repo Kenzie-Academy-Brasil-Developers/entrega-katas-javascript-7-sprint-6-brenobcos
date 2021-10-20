5e9c17b9e393f3865bd05de9b47dafbbb7c49942
Breno Carlos <bcos182@gmail.com>
2021-10-20 00:36:29 -0300

    first commit

---
+++ katas7.js
@@ -0,0 +1,115 @@
+function newForEach(array, callback){
+    for (let i = 0; i < array.length; i++){
+      callback(array[i], i, array)
+    }
+}
+
+
+function newMap(array, callback){
+    let newArray = []
+
+    for(let i = 0; i < array.length; i++){
+
+      const result = callback(array[i], i, array)
+      newArray.push(result)
+
+    }
+    return newArray
+}
+
+
+function newFilter(array,callback){
+  let result = []
+
+  for (let element of array){
+
+      if (callback(element)){
+          result.push(element)
+      }
+  }
+  return result
+}
+
+
+function newFind(array, callback){
+  let result
+  
+  for(let i = 0; i < array.length; i++){
+
+      result = callback(array[i], array)
+
+      if(result === true){
+          return array[i]
+      }
+  }
+}
+
+
+function newFindIndex(array,callback){
+  
+  for(let i = 0 ; i < array.length; i++){
+
+    if(callback(array[i])){
+      return i
+    }
+  }
+}
+
+
+function newReduce(array, callback, initialValue){
+  let acc = initialValue || 0
+
+  for(let i = 0; i < array.length; i++){
+    acc = callback(acc, array[i])
+  } 
+  
+  return acc 
+}
+
+
+function newSome(array, callback){
+  let result
+
+  for(let i = 0; i < array.length; i++){
+    result = callback(array[i], i, array)
+
+  }
+
+  return result
+}
+
+
+function newEvery(array, callback){
+  for(let i = 0; i < array.length; i++){
+    if(callback([array[i]]) === false) {
+      return false
+    }
+  }
+  return true
+}
+
+
+function newFill(array, num, start = 0, end = array.length){
+
+  for( ; start < end; start++){
+
+    array[start] = num
+  }
+
+  return array
+}
+
+
+function newIncludes(array, num, start = 0){
+  for( ; start < array.length; start++){
+    if(array[start] === num){
+      return true
+    }
+  }
+  return false
+}
+
+
+// function newIndexOf(){}
+// function newConcaz(){}
+// function newJoin(){}
